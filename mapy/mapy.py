a0be90545f2909e6c179d81983a602751e0cac47
Gilad <g.tsehori@gmail.com>
2018-10-05 16:23:40 +0300

    get_currency added
    The function gets the country's currency name. Gets rid of the initials (USD for example).

--- mapy/mapy.py
+++ mapy/mapy.py
@@ -28,8 +28,7 @@ class Mapy:
         """
         parser.add_argument('--country', '-c', action='store',
                             help='You may enter a country '
-                                 'name to test yourself!',
-                            nargs='+',
+                                 'name to test yourself!', nargs='+',
                             type=config.check_valid_country)
         args = parser.parse_args()
 
@@ -37,7 +36,7 @@ class Mapy:
         if args.country is not None:
             args.country = ' '.join(args.country)
             if args.country.lower() not in config.LIST_OF_AVAILABLE_COUNTRIES:
-                config.invalid_country_exception(args.country,
+                config.invalid_country_exception(country_name=args.country,
                                                  msg="{} is an incomplete "
                                                      "country name!")
         return args
@@ -49,9 +48,13 @@ class Mapy:
         self.set_vars(args=args)
         while True:
             self.init_country_details()
+
+            # Game ends
             play_or_not = input('\nDo you want to play another game? (y\\n)\n')
             if play_or_not != 'y':
                 break
+
+            # Another game; get another country name to play
             self.randomize_country()
 
     def set_vars(self, args):
@@ -61,31 +64,43 @@ class Mapy:
         if args.country:
             self.country = Country(name=' '.join(args.country))
         else:
+
+            # Note that randomize_country also sets self.country to a value
             self.randomize_country()
         self.country.name = self.country.name.lower()
 
     def randomize_country(self):
-        # print(config.LIST_OF_AVAILABLE_COUNTRIES)
         self.country = Country(name=random.choice(
             config.LIST_OF_AVAILABLE_COUNTRIES))
 
     def init_country_details(self):
         self.get_neighboring_countries()
+        self.get_currency()
+
+    def enter_country_details_page(self):
+        # Initialize a browser object and open Geonames countries page
+        browser = RoboBrowser(parser='html.parser')
+        browser.open(config.GEONAMES_COUNTRIES)
+
+        # Look for the link to the desired country page
+        formatted_country_name = self.country.name.replace(' ', '-')
+        country_details_page = config.GEONAMES_HOMEPAGE + \
+                               browser.select('.restable')[0]. \
+                                   find('a', href=re.compile
+                               (formatted_country_name))['href']
+        browser.open(country_details_page)
+        return browser
 
     def get_neighboring_countries(self):
-            # Initialize a browser object and open Geonames countries page
-            browser = RoboBrowser(parser='html.parser')
-            browser.open(config.GEONAMES_COUNTRIES)
+        browser = self.enter_country_details_page()
 
-            # Look for the link to the desired country page
-            formatted_country_name = self.country.name.replace(' ', '-')
-            country_details_page = config.GEONAMES_HOMEPAGE + \
-                                       browser.select('.restable')[0].\
-                                       find('a', href=re.compile(formatted_country_name))['href']
-            browser.open(country_details_page)
+        # Adding the current's country neighbors to the property neighbors
+        self.country.neighbors.extend([
+            country_tag.string for country_tag in browser.find_all('a')
+            if str(country_tag.string).lower() in
+               config.LIST_OF_AVAILABLE_COUNTRIES
+            and str(country_tag.string).lower() != self.country.name])
 
-            # Adding the current's country neighbors to the property neighbors
-            self.country.neighbors.extend([
-                country_tag.string for country_tag in browser.find_all('a')
-                if str(country_tag.string).lower() in config.LIST_OF_AVAILABLE_COUNTRIES
-                and str(country_tag.string).lower() != self.country.name])
+    def get_currency(self):
+        browser = self.enter_country_details_page()
+        return ' '.join(str(browser.find(string=re.compile(r'\([A-Z]'))).split()[:-1])
