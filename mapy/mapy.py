cdb5323b73372de8ba93bf713b8d87e8f7862e0b
Gilad <g.tsehori@gmail.com>
2018-10-07 16:44:53 +0300

    Game created
    The main loop for the game is initialized.
    A certain bug now exists for countries that are entered as arguments with -c; they're shown with only capital letters.

--- mapy/mapy.py
+++ mapy/mapy.py
@@ -11,6 +11,10 @@ import re
 
 
 class Country:
+    """
+    The Country class contains all the info about the country that's being
+    played in the moment; either chosen by the user or selected randomly.
+    """
     def __init__(self, name):
         self.name = name
         self.capital = None
@@ -42,6 +46,14 @@ class Mapy:
                                                      "country name!")
         return args
 
+    prettify = lambda self, arg: \
+        ' '.join([word[:1].upper() + word[1:]
+        if word.lower() not in
+        ['and', 'of', 'the']
+        else word[:1].lower() + word[1:]
+        for word in
+        re.split('\W+', arg)])
+
     def run(self, args):
         """
         Runs the program
@@ -50,6 +62,53 @@ class Mapy:
         while True:
             # Game starts
             self.init_country_details()
+
+            # Reformat fields to be pretty
+            self.country.name = self.prettify(self.country.name)
+            self.country.currency = self.prettify(self.country.currency)
+
+            # Greeting
+            print('Hello, welcome to {}!'.format(self.country.name))
+
+            # Currency question
+            currency_answer = input('What currency is used here? ')
+            if currency_answer.lower() == self.country.currency.lower():
+                print('You got it right! The currency is indeed {}!'.format(
+                    self.country.currency))
+            else:
+                print("Whoops, you got it wrong!\nThe currency is not {}, "
+                      "but {}!\nNot to worry, you'll get it next "
+                      "time".format(currency_answer, self.country.currency))
+
+            # Neighbors question
+            print("It's time to test you with neighbors!\nLet's see if you"
+                  " remember all of the countries that share a border with {}:"
+                  "\n(enter them one by one. If the country has no neighbors, "
+                  "write `none`.)\n".format(self.country.name))
+            neighbors_list_so_far = list()
+            if len(self.country.neighbors) != 0:
+                for i in range(len(self.country.neighbors)):
+                    neighbor_answer = input("Enter a neighbor country's name: ")
+                    print(self.country.neighbors)  # todo delete
+                    if neighbor_answer.lower() in self.country.neighbors:
+                        if neighbor_answer.lower() not in neighbors_list_so_far:
+                            neighbors_list_so_far.extend(neighbor_answer.lower())
+                            print("Yes! It is indeed a neighbor!")
+                            if i != len(self.country.neighbors):
+                                print("Let's continue, we're not done yet.")
+                        else:
+                            print("You've already listed this country, try again!")
+                            i -= 1
+                    else:
+                        print("Whoops, this country is not a neigbor! Sorry!")
+                        break
+            else:
+                no_neighbors_answer = input("Enter a neighbor country's name: ")
+                if no_neighbors_answer.lower() != 'none':
+                    print("This country has no neighbors at all! Next time,"
+                          " write `none`.")
+                else:
+                    print("You're right!!")
 
             # Game ends
             play_or_not = input('\nDo you want to play another game? (y\\n)\n')
@@ -85,10 +144,10 @@ class Mapy:
         browser.open(config.GEONAMES_COUNTRIES)
 
         # Look for the link to the desired country page
-        formatted_country_name = self.country.name.replace(' ', '-')
+        formatted_country_name = self.country.name.replace(' ', '')
         country_details_page = config.GEONAMES_HOMEPAGE + \
                                browser.select('.restable')[0]. \
-                                   find('a', href=re.compile
+                               find('a', href=re.compile \
                                (formatted_country_name))['href']
         browser.open(country_details_page)
         return browser
@@ -98,7 +157,7 @@ class Mapy:
 
         # Adding the current's country neighbors to the property neighbors
         self.country.neighbors.extend([
-            country_tag.string for country_tag in browser.find_all('a')
+            country_tag.string.lower() for country_tag in browser.find_all('a')
             if str(country_tag.string).lower() in
                config.LIST_OF_AVAILABLE_COUNTRIES
             and str(country_tag.string).lower() != self.country.name])
@@ -106,4 +165,4 @@ class Mapy:
     def get_currency(self):
         browser = self.enter_country_details_page()
         self.country.currency = ' '.join(str(browser.find(
-            string=re.compile(r'\([A-Z]'))).split()[:-1])
+            string=re.compile(r'\([A-Z]'))).split()[:-1]).lower()
